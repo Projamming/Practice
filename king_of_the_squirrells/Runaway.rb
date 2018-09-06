a8b54cbc05a63e2c318ca9d03f32cc2157556aff
king-of-the-squirrells <42661387+king-of-the-squirrells@users.noreply.github.com>
2018-09-05 22:45:00 -0400

    Create Runaway.rb

---
+++ king_of_the_squirrells/Runaway.rb
@@ -0,0 +1,48 @@
+#Runaway opening
+#By Kayne West
+#First working code :)
+14.times do
+  use_synth:piano
+  play:e6
+  sleep 1.3
+end
+1.times do
+  use_synth:piano
+  play:e5
+  sleep 1.3
+end
+3.times do
+  use_synth:piano
+  play:d6
+  sleep 1.3
+end
+1.times do
+  use_synth:piano
+  play:e5
+  sleep 1.3
+end
+3.times do
+  use_synth:piano
+  play:c6
+  sleep 1.3
+end
+1.times do
+  use_synth:piano
+  play:e5
+  sleep 1.3
+end
+3.times do
+  use_synth :piano
+  play:c6
+  sleep 1.3
+end
+1.times do
+  use_synth:piano
+  play:e5
+  sleep 1.3
+end
+1.times do
+  use_synth:piano
+  play:e6
+  sleep 1.3
+end 
