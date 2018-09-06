199b5ce2969b8f82ddc3a79ee8c42c16cb96f0a7
djbwac <42854034+djbwac@users.noreply.github.com>
2018-09-06 17:28:47 -0400

    Add files via upload

---
+++ djbwac/doorbell.rb
@@ -0,0 +1,57 @@
+#doorbell
+use_bpm 60
+
+play:C
+sleep 0.5
+play:E
+sleep 0.5
+play:A
+sleep 0.5
+play:F
+sleep 0.5
+play:C
+sleep 0.5
+play:G
+sleep 0.5
+play:F
+sleep 0.5
+play:G
+sleep 0.5
+play:A
+sleep 0.5
+play:D
+sleep 0.5
+play:C
+sleep 0.5
+play:G
+sleep 0.5
+play:C
+sleep 0.5
+play:C
+sleep 0.5
+play:E
+sleep 0.5
+play:A
+sleep 0.5
+play:C
+sleep 0.5
+play:F
+sleep 0.5
+play:C
+sleep 0.5
+play:G
+sleep 0.5
+play:F
+sleep 0.5
+play:G
+sleep 0.5
+play:A
+sleep 0.5
+play:D
+sleep 0.5
+play:C
+sleep 0.5
+play:G
+sleep 0.5
+play:C
+sleep 0.5
