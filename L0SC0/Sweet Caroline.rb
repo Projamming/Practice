3a24119dec75fdd8beddb41929a6a134bc82d702
L0SC0 <42723212+L0SC0@users.noreply.github.com>
2018-09-12 19:55:14 -0400

    Add files via upload

---
+++ L0SC0/Sweet Caroline.rb
@@ -0,0 +1,92 @@
+#Title   :Sweet Caroline
+#File   :
+#Author :L0SC0
+#Date : Sept 9, 2018
+#Descr.
+
+use_bpm 128
+
+use_synth :piano
+
+play :E3
+sleep 1
+play :F3
+sleep 1
+play :G3
+sleep 1
+play :G3, sustain:3
+play :E3, sustain:3
+sleep 3
+
+play :D3
+sleep 1
+play :E3
+sleep 1
+play :F3
+sleep 1
+play :F3
+sleep 1
+play :F3
+sleep 0.5
+play :E3, sustain:1
+sleep 1
+play :D3
+sleep 1
+
+play :C3
+sleep 3
+play :D3
+sleep 0.5
+play :E3
+sleep 0.5
+play :E3
+sleep 1
+play :E3
+sleep 1
+play :D3
+sleep 1
+play :C3
+sleep 1
+play :G2, sustain:4
+sleep 4
+
+play :E3, sustain:5
+play :G3, sustain:5
+sleep 5
+play :E3, sustain:5
+play :G3, sustain:5
+sleep 1
+play :A3
+sleep 0.5
+play :A3
+sleep 0.5
+play :A3, sustain:3
+sleep 3
+
+play :D3, sustain:6
+play :F3, sustain:6
+sleep 0.5
+play :B3
+sleep 0.5
+play :B3
+sleep 0.5
+play :B3, sustain:1
+sleep 3
+
+play :F3, sustain:6
+play :A3, sustain:6
+sleep 0.5
+play :C4
+sleep 0.5
+play :C4
+sleep 0.5
+play :C4, sustain:3
+sleep 3
+
+play :C4
+sleep 1
+play :D4
+sleep 1
+play :G3, sustain:3, amp:5
+play :B3, sustain:3, amp:5
+play :D4, sustain:3, amp:5
