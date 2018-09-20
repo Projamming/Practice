f2e52b0cecd874cb83f1a7422e023c05db33835e
Queen-Pin <42684526+Queen-Pin@users.noreply.github.com>
2018-09-20 17:49:15 -0400

    Update messing_around.rb

--- Queen-Pin/messing_around.rb
+++ Queen-Pin/messing_around.rb
@@ -1,5 +1,6 @@
 use_bpm 120
-d
+use_synth :piano
+
 def melody
   notes = [:g3, :d4, :f4, :g4, :g4, :fs4, :fs4,
            :g4, :fs4, :e4, :d4, :r]
@@ -13,9 +14,16 @@ def harmony
            :b4, :a4, :g3, :g3, :r]
   duration = [0.5, 0.5, 0.5, 1, 0.5, 1,
               1, 1, 0.5, 0.5, 0.5, 1.5]
+  play_pattern_timed notes, duration
 end
-live_loop :beat do
-  use_synth :pluck
-  melody
+
+in_thread do
+  loop do
+    melody
+    sleep 0.25
+  end
+end
+loop do
   harmony
+  sleep 0.25
 end
